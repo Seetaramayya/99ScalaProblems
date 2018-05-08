2d75be9c87867f7bc6c28c453a1dcf0742557719
Seeta <srvadali@gmail.com>
2018-05-08 23:11:08 +0200

    P16 property based test added

--- src/main/scala/com/seeta/problems/lists/P16.scala
+++ src/main/scala/com/seeta/problems/lists/P16.scala
@@ -5,9 +5,12 @@ object P16 {
     * P 16. Drop every Nth element from a list.
     */
   def drop[T](position: Int, input: List[T]): List[T] = {
-    if (position <= 0 ) throw new IllegalArgumentException("position should be positive number")
-    input.zipWithIndex.filterNot {
-      case (_, index) => (index + 1) % position == 0
-    }.map(_._1)
+    if (position <= 0 ) {
+      input
+    } else {
+      input.zipWithIndex.filterNot {
+        case (_, index) => (index + 1) % position == 0
+      }.map(_._1)
+    }
   }
 }
--- src/test/scala/com/seeta/problems/lists/DropPropertySpecification.scala
+++ src/test/scala/com/seeta/problems/lists/DropPropertySpecification.scala
@@ -11,23 +11,29 @@ class DropPropertySpecification extends WordSpec with PropertyChecks with Matche
   val position: Gen[Int] = Gen.choose(1, 100)
   val generatedIntList: Gen[List[Int]] = Gen.listOfN(Random.nextInt(1000), Gen.choose(-1000, 1000))
   "Drop" should {
-    "hold the property: output + dropped elements == input" in {
+
+    "hold the property: all dropped elements divisible by position" in {
       forAll(position, generatedIntList) { (position: Int, generatedIntList: List[Int]) =>
         val output = drop(position, generatedIntList)
         if (position == 1) {
           output shouldBe List()
         } else {
-          val groupedElements: List[List[Int]] = output.grouped(position - 1).toList
-          val missingElementsPos: List[Option[Int]] = (1 to generatedIntList.length).filter(_ % position == 0).map(Some(_)).toList
-          val missingElements: List[Option[Int]] = missingElementsPos.map {
-            case Some(idx) => Some(generatedIntList(idx))
-          }
-          println(missingElements)
-          val combinedElements: List[(List[Int], Option[Int])] = groupedElements.zipAll(missingElements, List(), None)
-          combinedElements.flatMap {
-            case (list, Some(element)) => list :+ element
-            case (list, None)          => list
-          } shouldBe generatedIntList
+          val missingElements = generatedIntList.filter(output.contains(_))
+          missingElements.forall(_ % position == 0)
+        }
+      }
+    }
+
+    "hold the property: input size == (result + dropped elements) size" in {
+      forAll(position, generatedIntList) { (position: Int, generatedIntList: List[Int]) =>
+        val result = drop(position, generatedIntList)
+        if (position >= 1) {
+          val inputSize = generatedIntList.size
+          val zeroIndexPosition = position - 1
+          val positions: List[Int] = (zeroIndexPosition until inputSize by position).toList
+          result.size + positions.size shouldBe inputSize
+        } else {
+          result shouldBe generatedIntList
         }
       }
     }
